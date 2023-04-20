2f32d98c407225e9568e0a2e84410c12edf82afb
hong <hki007@hanmail.net>
2023-04-20 14:37:51 +0900

    20230419-32강 완료 commit

--- src/main/java/com/study/domain/comment/CommentApiController.java
+++ src/main/java/com/study/domain/comment/CommentApiController.java
@@ -1,10 +1,9 @@
 package com.study.domain.comment;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +15,12 @@ public class CommentApiController {
     public CommentResponse saveComment(@PathVariable final Long postId, @RequestBody final CommentRequest params) {
         Long id = commentService.saveComment(params);
         return commentService.findCommentById(id);
+    }
+
+    // 댓글 리스트 조회
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentResponse> findAllComment(@PathVariable final Long postId) {
+        return commentService.findAllComment(postId);
     }
 
 }
